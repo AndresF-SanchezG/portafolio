ee9391cca64ac74971e1c4ddbdcaf0b004c49b7c
AndresF-sanchezG <e.digitalcolombia@gmail.com>
2023-08-04 21:06:34 -0500

    V32

--- certifications.js
+++ certifications.js
@@ -1,15 +1,19 @@
 let certificationContainer = document.getElementById('certification-conteiner');
 let javaScriptCertificationContainer = document.getElementById('javaScriptCertificationContainer');
 let librariesCertificationContainer = document.getElementById('librariesCertificationContainer');
+let dbCertificationContainer = document.getElementById('DBCertificationContainer');
+let backendCertContainer = document.getElementById('backEndCertificationContainer');
 let responsiveCertificationContainer = document.getElementById('responsiveCertificationContainer');
 let responsiveContainer = document.getElementById('responsiveContainer');
 let javaScriptContainer = document.getElementById('javaScriptContainer');
 let librariesContainer = document.getElementById('librariesContainer');
+let DBContainer = document.getElementById('dBContainer');
+let BKContainer = document.getElementById('backendContainer');
 let readLessCertification = document.getElementById('readLessCertification-container');
 let readLessCertificationJS = document.getElementById('readLessCertificationJs-container');
 let readLessCertificationRT = document.getElementById('readLessCertificationRT-container');
-
-
+let readLessCertificationDB = document.getElementById('readLessCertificationDB-container');
+let readLessCertificationBK = document.getElementById('readLessCertificationBackend-container');
 
 initialize()
 
@@ -28,11 +32,9 @@ async function initialize() {
 
     crearTablaResponsive(certifications);
     crearTablaJavaScript(certifications);
-    crearTablaLibraries(certifications)
-
-    
-    
-
+    crearTablaLibraries(certifications);
+    crearTablaDataBase(certifications);
+    crearTablaBackend(certifications);
 }
 
 function crearTablaResponsive(certifications) {
@@ -68,8 +70,7 @@ function crearTablaResponsive(certifications) {
 
     responsiveContainer.style.display = 'none';
     readLessCertification.style.display = 'none';
-    
-    
+
 }
 function crearTablaJavaScript(certifications) {
     // let propiedades = Object.getOwnPropertyNames(certifications[0]);
@@ -86,28 +87,21 @@ function crearTablaJavaScript(certifications) {
         tableJS.appendChild(rowsTable);
 
           
-           let td1 = document.createElement('td');
-           td1.textContent = certifications[1].certifications[i].name; 
-           rowsTable.appendChild(td1);
+            let td1 = document.createElement('td');
+            td1.textContent = certifications[1].certifications[i].name; 
+            rowsTable.appendChild(td1);
    
-          
-           let td2 = document.createElement('td');
-       
-        rowsTable.appendChild(td2); 
+            let td2 = document.createElement('td');
+            rowsTable.appendChild(td2); 
 
-        let ancle = document.createElement('a');
-        ancle.textContent = 'Certification';
-        ancle.setAttribute('href', certifications[1].certifications[i].url);
-        ancle.setAttribute('class', 'ancle-certification');
-        td2.appendChild(ancle);
-        
-
-    }
+            let ancle = document.createElement('a');
+            ancle.textContent = 'Certification';
+            ancle.setAttribute('href', certifications[1].certifications[i].url);
+            ancle.setAttribute('class', 'ancle-certification');
+            td2.appendChild(ancle);
+        }
 
     javaScriptContainer.style.display = 'none'
-    
-    
-    
 }
 
 function crearTablaLibraries(certifications) {
@@ -139,14 +133,77 @@ function crearTablaLibraries(certifications) {
         ancle.setAttribute('href', certifications[2].certifications[i].url);
         ancle.setAttribute('class', 'ancle-certification');
         td2.appendChild(ancle);
-        
-
     }
 
-    librariesContainer.style.display = 'none'
+    librariesContainer.style.display = 'none' 
+}
+
+function crearTablaDataBase(certifications) {
+    // let propiedades = Object.getOwnPropertyNames(certifications[0]);
+    // let cantColumnas = 2;
+    // console.log(propiedades)
+    let tableDB = document.createElement('table');
+    DBContainer.appendChild(tableDB); 
+    let certificationsCount = certifications[3].certifications.length;
+    console.log(certificationsCount);
     
+
+    for(let i = 0; i<certificationsCount; i++) {
+        let rowsTable = document.createElement('tr');
+        tableDB.appendChild(rowsTable);
+
+          
+           let td1 = document.createElement('td');
+           td1.textContent = certifications[3].certifications[i].name; 
+           rowsTable.appendChild(td1);
+   
+          
+           let td2 = document.createElement('td');
+       
+        rowsTable.appendChild(td2); 
+
+        let ancle = document.createElement('a');
+        ancle.textContent = 'Certification';
+        ancle.setAttribute('href', certifications[3].certifications[i].url);
+        ancle.setAttribute('class', 'ancle-certification');
+        td2.appendChild(ancle);
+    }
+
+    DBContainer.style.display = 'none' 
+}
+
+function crearTablaBackend(certifications) {
+    // let propiedades = Object.getOwnPropertyNames(certifications[0]);
+    // let cantColumnas = 2;
+    // console.log(propiedades)
+    let tableBK = document.createElement('table');
+    BKContainer.appendChild(tableBK); 
+    let certificationsCount = certifications[4].certifications.length;
+    console.log(certificationsCount);
     
-    
+
+    for(let i = 0; i<certificationsCount; i++) {
+        let rowsTable = document.createElement('tr');
+        tableBK.appendChild(rowsTable);
+
+          
+           let td1 = document.createElement('td');
+           td1.textContent = certifications[4].certifications[i].name; 
+           rowsTable.appendChild(td1);
+   
+          
+           let td2 = document.createElement('td');
+       
+        rowsTable.appendChild(td2); 
+
+        let ancle = document.createElement('a');
+        ancle.textContent = 'Certification';
+        ancle.setAttribute('href', certifications[4].certifications[i].url);
+        ancle.setAttribute('class', 'ancle-certification');
+        td2.appendChild(ancle);
+    }
+
+    BKContainer.style.display = 'none' 
 }
 
 function toggleVisibility(element, readLessButton) {
@@ -159,7 +216,6 @@ function toggleVisibility(element, readLessButton) {
     }
 }
 
-
 responsiveCertificationContainer.addEventListener('click', () => {
     toggleVisibility(responsiveContainer, readLessCertification);
 });
@@ -167,11 +223,19 @@ responsiveCertificationContainer.addEventListener('click', () => {
 // Aquí definimos javaScriptCertificationContainer
 
 javaScriptCertificationContainer.addEventListener('click', () => {
-    toggleVisibility(javaScriptContainer, readLessCertificationJS); // Actualiza el botón "Read Less" correspondiente
+    toggleVisibility(javaScriptContainer, readLessCertificationJS); 
 });
 
 librariesCertificationContainer.addEventListener('click', () => {
-    toggleVisibility(librariesContainer, readLessCertificationRT); // Actualiza el botón "Read Less" correspondiente
+    toggleVisibility(librariesContainer, readLessCertificationRT); 
+});
+
+dbCertificationContainer.addEventListener('click', () => {
+    toggleVisibility(DBContainer, readLessCertificationDB); 
+});
+
+backendCertContainer.addEventListener('click', () => {
+    toggleVisibility(BKContainer, readLessCertificationBK); 
 });
 
 readLessCertification.addEventListener('click', () => {
@@ -179,11 +243,15 @@ readLessCertification.addEventListener('click', () => {
 });
 
 readLessCertificationJS.addEventListener('click', () => {
-    toggleVisibility(javaScriptContainer, readLessCertificationJS); // Actualiza el botón "Read Less" correspondiente
+    toggleVisibility(javaScriptContainer, readLessCertificationJS); 
 });
 
 readLessCertificationRT.addEventListener('click', () => {
-    toggleVisibility(librariesContainer, readLessCertificationRT); // Actualiza el botón "Read Less" correspondiente
+    toggleVisibility(librariesContainer, readLessCertificationRT); 
+});
+
+readLessCertificationBK.addEventListener('click', () => {
+    toggleVisibility(BKContainer, readLessCertificationBK); 
 });
 
 
